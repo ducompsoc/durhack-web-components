dc975554a67a0ded18188da26527c9f98e76501c
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-06-22 02:49:58 +0100

    tweak form element styles to avoid iOS zooming in on fields

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -22,6 +22,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-9 px-4 py-2",
+        form: "h-10 lg:h-9 px-4 py-2 text-base lg:text-sm",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
