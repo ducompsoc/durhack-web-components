0417891b0da92d12fbe49ce8f97b0b35c6be380e
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-06-19 09:51:36 +0100

    Remove `forwardRef` from `checkbox.tsx`

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -6,12 +6,14 @@ import { CheckIcon, DashIcon } from "@radix-ui/react-icons"
 
 import { cn } from "@/lib/utils"
 
-const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
+function Checkbox(
+  {
+    className,
+    ...props
+  }: React.ComponentProps<typeof CheckboxPrimitive.Root>
+) {
+  return <CheckboxPrimitive.Root
+    data-slot="checkbox"
     className={cn(
       "peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow-sm focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className
@@ -21,11 +23,10 @@ const Checkbox = React.forwardRef<
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current group")}
     >
-      <CheckIcon className="h-4 w-4 group-data-[state=indeterminate]:hidden" />
-      <DashIcon className="h-4 w-4 hidden group-data-[state=indeterminate]:block " />
+      <CheckIcon className="h-4 w-4 group-data-[state=indeterminate]:hidden"/>
+      <DashIcon className="h-4 w-4 hidden group-data-[state=indeterminate]:block "/>
     </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+  </CheckboxPrimitive.Root>;
+}
 
 export { Checkbox }
