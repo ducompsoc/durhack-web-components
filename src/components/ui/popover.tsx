4c7b721810ad177bbc9c03d7ae6d6a4116f4af94
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-06-19 10:12:52 +0100

    Remove `forwardRef` from `popover.tsx`

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -11,13 +11,17 @@ const PopoverTrigger = PopoverPrimitive.Trigger
 
 const PopoverAnchor = PopoverPrimitive.Anchor
 
-const PopoverContent = React.forwardRef<
-  React.ElementRef<typeof PopoverPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Portal>
+function PopoverContent(
+  {
+    className,
+    align = "center",
+    sideOffset = 4,
+    ...props
+  }: React.ComponentProps<typeof PopoverPrimitive.Content>
+) {
+  return <PopoverPrimitive.Portal>
     <PopoverPrimitive.Content
-      ref={ref}
+      data-slot="popover-content"
       align={align}
       sideOffset={sideOffset}
       className={cn(
@@ -26,8 +30,7 @@ const PopoverContent = React.forwardRef<
       )}
       {...props}
     />
-  </PopoverPrimitive.Portal>
-))
-PopoverContent.displayName = PopoverPrimitive.Content.displayName
+  </PopoverPrimitive.Portal>;
+}
 
 export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
